3832ec04e044e7b652f0b336d9fcbd6175b5a787
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-10 15:04:58 +0200

    troubleshooting

---
+++ guessing game.py
@@ -0,0 +1,15 @@
+
+secret_word = "loudness"
+guess = ""
+i = 1
+
+while guess != secret_word:
+    guess = input("Enter guess: ")
+    if i <= 2:
+        i += 1
+        print("try again,")
+    elif i == 2:
+        break
+
+
+print("You've learned the secret!")
